88a53f296e40d1fb84ecf8c599db5b06c8571783
Chiara <chiaraiors@gmail.com>
2022-01-11 16:42:05 +0100

    add movie card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,11 +10,23 @@
 
         <div class="movie">
             <h3>{{$movie->title}}</h3>
-            <h4>{{$movie->original_title}}</h4>
-
-            <p>{{$movie->nationality}}</p>
-            <p>{{$movie->date}}</p>
-            <p>{{$movie->vote}}</p>
+            <h4>
+                Titolo originale:
+                <span class="italic">{{$movie->original_title}}</span>
+            </h4>
+            <p>
+                <span class="bold">Nazionalità:</span>
+                @if($movie->nationality === 'american')
+                <img src="{{asset('img/usa.png')}}" alt="usa">
+                @elseif($movie->nationality === 'american/british')
+                <img src="{{asset('img/usa.png')}}" alt="usa">
+                <img src="{{asset('img/gb.svg')}}" alt="gb">
+                @else
+                <img src="{{asset('img/gb.svg')}}" alt="gb">
+                @endif
+            </p>
+            <p><span class="bold">Data di uscita:</span> {{$movie->date}}</p>
+            <p><span class="bold">Punteggio:</span> {{$movie->vote}}/10</p>
         </div>
 
         @empty
